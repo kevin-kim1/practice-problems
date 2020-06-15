2bfa4ca94790aa8a46d198bc09ddd4101c321d3c
Kevin Kim <kevinnhkim@live.com>
2020-06-15 11:36:11 -0700

    Completed Convenience Store

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function toArray(obj) {
   }
   return resultArr;
 }
+
+// https://edabit.com/challenge/erFxBbqzZPSegMwnc
+// Change Enough
+function changeEnough(change, amountDue) {
+  let total = 0;
+  let diffTotal = 0
+  for (let i = 0; i < change.length; i++) {
+    if (i === 0) {
+      total += .25 * change[i]
+    } else if (i === 1) {
+      total += .10 * change[i]
+    } else if (i === 2) {
+      total += .5 * change[i]
+    } else {
+      total += .01 * change[i]
+    }
+  }
+  diffTotal = total - amountDue;
+  if (diffTotal > 0) {
+    return true
+  } else {
+    return false;
+  }
+}
