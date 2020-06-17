087ba3ea009e561dd13cd645999e9a7e083b6bdf
Kevin Kim <kevinnhkim@live.com>
2020-06-17 10:05:45 -0700

    Completed algo for creating phone number format from numbers in an array

--- index.js
+++ index.js
@@ -32,3 +32,22 @@ function changeEnough(change, amountDue) {
     return false;
   }
 }
+// https://edabit.com/challenge/xDNkweBBNXBMyQRdG
+// Create a phone number
+
+function createPhoneNumber(numbers) {
+  let phoneNum = "";
+  for (let i = 0; i < numbers.length; i++) {
+    if (i === 0) {
+      phoneNum += "(" + numbers[i];
+    } else if (i === 2) {
+      phoneNum += numbers[i] + ")"
+    } else if (i === 3) {
+      phoneNum += " " + numbers[i];
+    } else if (i === 6) {
+      phoneNum += "-" + numbers[i];
+    } else {
+      phoneNum += numbers[i];
+    }
+  }
+  return phoneNum;
