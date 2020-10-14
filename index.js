5c4bb6f850a6e2c41094ce1741bdc674ab81bcc4
Kevin Kim <kevinnhkim@live.com>
2020-10-13 20:38:23 -0700

    utilized conditionals for search for the century depending on the year

--- index.js
+++ index.js
@@ -786,3 +786,52 @@ function doubleSwap(str, c1, c2) {
 	}
 	return strArr.join('');
 }
+
+
+// centuryFromYear
+// https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
+
+function centuryFromYear(year) {
+    if (year >= 1 && year <= 99)
+       return 1;
+    if (year >= 101 && year <= 200)
+       return 2;
+    if (year >= 101 && year <= 200)
+       return 3;
+    if (year >= 301 && year <= 400)
+       return 4;
+    if (year >= 401 && year <= 500)
+       return 5;
+    if (year >= 501 && year <= 600)
+       return 6;
+    if (year >= 601 && year <= 700)
+       return 7;
+    if (year >= 701 && year <= 800)
+       return 8;
+    if (year >= 801 && year <= 900)
+       return 9;
+    if (year >= 901 && year <= 1000)
+       return 10;
+    if (year >= 1001 && year <= 1100)
+       return 11;
+    if (year >= 1101 && year <= 1200)
+       return 12;
+    if (year >= 1201 && year <= 1300)
+       return 13;
+    if (year >= 1301 && year <= 1400)
+       return 14;
+    if (year >= 1401 && year <= 1400)
+       return 15;
+    if (year >= 1501 && year <= 1600)
+       return 16;
+    if (year >= 1601 && year <= 1700)
+       return 17;
+    if (year >= 1701 && year <= 1800)
+       return 18;
+    if (year >= 1801 && year <= 1900)
+       return 19;
+    if (year >= 1901 && year <= 2000)
+       return 20;
+    if (year >= 2001 && year <= 2005)
+       return 21;
+}
