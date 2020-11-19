a1fbc2f223913bd9ff238461a58d00e63adf3fc1
Kevin Kim <kevinnhkim@live.com>
2020-11-18 21:03:27 -0800

    turned s1 and s2 params into array using spilt method and created a s1 object and s2 object

--- index.js
+++ index.js
@@ -950,3 +950,14 @@ function allLongestStrings(inputArray) {
     });
     return inputArray;
 }
+
+
+// Common Character Count
+
+function commonCharacterCount(s1, s2) {
+  s1 = s1.split('');
+  s2 = s2.split('');
+  let s1Obj = {};
+  let s2Obj = {};
+
+}
